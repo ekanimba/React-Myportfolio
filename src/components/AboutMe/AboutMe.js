cb38b3c814be5991f5070b032bf9a9aa79d91e06
ekaniz <ekanimba3@gmail.com>
2020-06-14 17:07:10 +0200

    Added About me Page with underconstruction

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,9 +6,11 @@ class AboutMe extends Component {
     return (
       <div className="aboutMe-container">
         <div className="aboutme-header">
-          <h1>About Me</h1>
+          <h1>This page is under Construction !</h1>
+          <div className="aboutme-image-container">
+          <img src="https://ekanimba-myportfolio.s3.eu-central-1.amazonaws.com/underconstruction.svg" alt=''/>
+          </div>
         </div>
-          
       </div>
     )
   }
